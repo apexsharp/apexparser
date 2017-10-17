daa0846d8e88c9fd6327758ab344ac86fbc5ddfc
Jay Janarthanan <jay@jayonsoftware.com>
2017-10-17 06:22:53 -0400

    Updated the ApexCode Gen

---
+++ ApexSharpBase/Converter/Apex/ApexCodeGenerator.cs
@@ -0,0 +1,98 @@
+﻿using System.Text;
+using ApexSharpBase.Ext;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace ApexSharpBase.Converter.Apex
+{
+    public class ApexCodeGenerator : CSharpSyntaxWalker
+    {
+        StringBuilder sb = new StringBuilder();
+
+        public ApexCodeGenerator() : base(SyntaxWalkerDepth.Trivia)
+        {
+        }
+
+        public string GetApexCode(string cSharpCode)
+        {
+            var syntaxTree = CSharpSyntaxTree.ParseText(cSharpCode);
+            Visit(syntaxTree.GetRoot());
+            return sb.ToString();
+        }
+
+        public string GetModifiers(SyntaxTokenList syntaxTokenList)
+        {
+            return syntaxTokenList[0].Text;
+        }
+
+        public string GetAttributes(SyntaxList<AttributeListSyntax> attributeList)
+        {
+            if (attributeList.Any()) return attributeList[0].ToString();
+            return "";
+        }
+
+        public string GetParametr(ParameterListSyntax parameterListSyntax)
+        {
+            StringBuilder parameterString  = new StringBuilder();
+            parameterString.Append(parameterListSyntax.OpenParenToken);
+            foreach (var parameter in parameterListSyntax.Parameters)
+            {
+                parameterString.Append(FieldConverter.GetApexTypes(parameter.Type.ToString()));
+                parameterString.AppendSpace();
+                parameterString.Append(parameter.Identifier);
+            }
+            parameterString.Append(parameterListSyntax.CloseParenToken);
+            return parameterString.ToString();
+        }
+
+        public override void Visit(SyntaxNode node)
+        {
+            switch (node.Kind())
+            {
+                case SyntaxKind.ClassDeclaration:
+                {
+                    sb.AppendLine("class {");
+                    base.Visit(node);
+                    sb.AppendLine("}");
+                    break;
+                }
+                case SyntaxKind.MethodDeclaration:
+                {
+                    var methodDeclaration = (MethodDeclarationSyntax) node;
+
+                    sb.Append(GetAttributes(methodDeclaration.AttributeLists));
+                    sb.AppendLine();
+                    sb.Append($"{GetModifiers(methodDeclaration.Modifiers)} {FieldConverter.GetApexTypes(methodDeclaration.ReturnType.ToString())} {methodDeclaration.Identifier.Text}{GetParametr(methodDeclaration.ParameterList)}");
+                    sb.AppendLine();
+
+                    sb.AppendLine(methodDeclaration.Body.OpenBraceToken.Text);
+                    base.Visit(node);
+                    sb.AppendLine(methodDeclaration.Body.CloseBraceToken.Text);
+                    break;
+                }
+                case SyntaxKind.LocalDeclarationStatement:
+                {
+                    sb.AppendLine("LocalDeclarationStatemen");
+                    base.Visit(node);
+                    break;
+                }
+                case SyntaxKind.IfStatement:
+                {
+                    sb.AppendLine("If {");
+                    base.Visit(node);
+                    sb.AppendLine("}");
+                    break;
+                }
+                default:
+                {
+                    //  Console.WriteLine(node.Kind());
+
+                    base.Visit(node);
+                    break;
+                }
+
+            }
+        }
+    }
+}
--- ApexSharpBase/Converter/CSharp/CSharpGenerator.cs
+++ ApexSharpBase/Converter/CSharp/CSharpGenerator.cs
@@ -1,108 +1,15 @@
 ﻿using System;
 using System.Linq;
 using ApexSharpBase.Ext;
+using ApexSharpBase.MetaClass;
+
+using System.Text;
+
 
 namespace ApexSharpBase.Converter.CSharp
 {
-    using System.Text;
-    using ApexSharpBase.MetaClass;
-
     public class CSharpGenerator
     {
-        public string GenerateWithIndentation(ClassContainer classContainer)
-        {
-            return Generate(classContainer);
-        }
-
-
-        public string Generate(ClassContainer classContainer)
-        {
-            StringBuilder sb = new StringBuilder();
-
-            GenerateCode(sb, classContainer);
-
-            return sb.ToString();
-        }
-
-
-        public string GetAttributes(ClassSyntax cs)
-        {
-            if (cs.Attributes.Any()) return cs.Attributes[0];
-            return "";
-        }
-
-        public string GetModifiers(ClassSyntax cs)
-        {
-            if (cs.Modifiers.Any()) return cs.Modifiers[0];
-            return "";
-        }
-
-        public string GetModifiers(MethodSyntax cs)
-        {
-            if (cs.Modifiers.Any()) return cs.Modifiers[0];
-            return "";
-        }
-
-        public string LangType { get; set; }
-        public void GenerateCode(StringBuilder sb, BaseSyntax baseSyntax)
-        {
-            if (baseSyntax.Kind == SyntaxType.ClassContainer.ToString())
-            {
-                var classContainer = (ClassContainer)baseSyntax;
-
-                LangType = classContainer.ContainerLang;
-
-                sb.AppendLine("namespace ApexSharpDemo.ApexCode");
-                sb.AppendLine("{");
-                //sb.AppendLine("using SObjects;");
-                //sb.AppendLine("using Apex.System;");
-                //sb.AppendLine("using Apex.ApexSharp;");
-                //sb.AppendLine("using Apex.ApexAttrbutes;");
-                //sb.AppendLine("using SalesForceAPI.Apex;");
-  
-                foreach (var childNode in classContainer.ChildNodes) GenerateCode(sb, childNode);
-
-                sb.Append("}");
-            }
-
-            else if (baseSyntax.Kind == SyntaxType.Class.ToString())
-            {
-                var classSyntex = (ClassSyntax)baseSyntax;
-                if(GetAttributes(classSyntex) != String.Empty) sb.Append($"[{GetAttributes(classSyntex)}]").AppendLine(); ;
-                sb.Append($"{GetModifiers(classSyntex)} class {classSyntex.Identifier}").AppendLine();
-                sb.AppendLine("{");
-
-                foreach (var childNode in baseSyntax.ChildNodes) GenerateCode(sb, childNode);
-
-                sb.AppendLine("}");
-            }
-            else if (baseSyntax.Kind == SyntaxType.Method.ToString())
-            {
-                var methodSyntax = (MethodSyntax) baseSyntax;
-
-                var returnType= methodSyntax.ReturnType;
-                if (LangType == "APEX")
-                {
-                    returnType = FieldConverter.GetApexTypes(methodSyntax.ReturnType);
-                }
-
-                sb.Append($"{GetModifiers(methodSyntax)} {returnType} {methodSyntax.Identifier}()")
-                    .AppendLine();
-                sb.AppendLine("{");
-
-                foreach (var childNode in baseSyntax.ChildNodes) GenerateCode(sb, childNode);
-
-                sb.AppendLine().Append("}").AppendLine();
-            }
-            else if (baseSyntax.Kind == SyntaxType.ReturnStatement.ToString())
-            {
-                var returnStatement = (ReturnStatement) baseSyntax;
-                sb.Append("return ").Append(returnStatement.Expression).Append(";");
-            }
-            else
-            {
-                sb.AppendLine("Could Not Generate Code For " + baseSyntax.Kind);
-            }
-        }
+    
     }
 }
