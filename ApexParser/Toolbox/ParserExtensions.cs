f583f97ef7620d52027029a1c0198d1d53a493be
yallie <yallie@yandex.ru>
2017-10-18 00:15:26 +0300

    Implemented the detailed ParseError messages. Please use ParseEx<T> extension method to get the complete invalid line of code.

---
+++ ApexParser/Toolbox/ParserExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sprache;
+
+namespace ApexParser.Toolbox
+{
+    public static class ParserExtensions
+    {
+        public static T ParseEx<T>(this Parser<T> parser, string input)
+        {
+            var result = parser.TryParse(input);
+            if (result.WasSuccessful)
+            {
+                return result.Value;
+            }
+
+            var message = result.ToString();
+
+            // append the whole current line text
+            var lines = (input ?? string.Empty).Split(new[] { "\r\n", "\n" }, StringSplitOptions.None);
+            var lineNumber = result.Remainder.Line - 1;
+            if (lineNumber > 0 && lineNumber < lines.Length)
+            {
+                var cr = Environment.NewLine;
+                message = $"{message}{cr}--- The current line: ---{cr}{lines[lineNumber]}";
+            }
+
+            throw new ParseException(message);
+        }
+    }
+}
---
+++ ApexParserTest/Toolbox/ParseExtensionTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ApexParser.Parser;
+using ApexParser.Toolbox;
+using NUnit.Framework;
+using Sprache;
+using static ApexParserTest.Properties.Resources;
+
+namespace ApexParserTest.Toolbox
+{
+    [TestFixture]
+    public class ParseExtensionTests
+    {
+        [Test]
+        public void ParseExProducesMoreDetailedExceptionMessage()
+        {
+            // append the error line to the valid demo file
+            var errorLine = "--===-- oops! --===--";
+            var demo = Demo + Environment.NewLine + errorLine;
+
+            try
+            {
+                new ApexGrammar().ClassDeclaration.End().ParseEx(demo);
+                Assert.Fail("The code should have thrown ParseException.");
+            }
+            catch (ParseException ex)
+            {
+                // check that the error message contains the complete invalid code line
+                Assert.NotNull(ex);
+                Assert.True(ex.Message.Contains(errorLine));
+            }
+        }
+    }
+}
