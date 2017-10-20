f1b132a9b65daaa730e8ec367deebf3c8775e701
yallie <yallie@yandex.ru>
2017-10-20 19:14:09 +0300

    Added perl script to generate the reserved words. Added the lists of reserved and non-reserved words from the official docs.

---
+++ ApexParser/Parser/keywords.pl
@@ -0,0 +1,15 @@
+﻿# Sample usage: perl -w keywords.pl reserved.txt > tmp.cs
+
+while (<>)
+{
+	next if /http/;
+	next if /^\s*$/;
+
+	s/\s*$//; # strip newlines
+	$reserved = s/\*// ? " // reserved for future use" : ""; # strip stars
+
+
+	$name = ucfirst($_);
+	$value = $_;
+	print "        public const string $name = \"$value\";$reserved\n";
+}
