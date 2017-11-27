f4d3c23700223d2fb284ded1d0a57959dd71c035
yallie <yallie@yandex.ru>
2017-11-27 14:55:57 +0300

    Added an Open file button.

--- Playground/ConvertorForm.Designer.cs
+++ Playground/ConvertorForm.Designer.cs
@@ -34,7 +34,9 @@
             this.SplitContainer = new System.Windows.Forms.SplitContainer();
             this.RightBox = new FastColoredTextBoxNS.FastColoredTextBox();
             this.TopPanel = new System.Windows.Forms.Panel();
+            this.OpenButton = new System.Windows.Forms.Button();
             this.ConversionButton = new System.Windows.Forms.Button();
+            this.OpenFileDialog = new System.Windows.Forms.OpenFileDialog();
             ((System.ComponentModel.ISupportInitialize)(this.LeftBox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.SplitContainer)).BeginInit();
             this.SplitContainer.Panel1.SuspendLayout();
@@ -68,7 +70,6 @@
             this.LeftBox.Cursor = System.Windows.Forms.Cursors.IBeam;
             this.LeftBox.DisabledColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))));
             this.LeftBox.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.LeftBox.Font = new System.Drawing.Font("Courier New", 9.75F);
             this.LeftBox.IsReplaceMode = false;
             this.LeftBox.Language = FastColoredTextBoxNS.Language.CSharp;
             this.LeftBox.LeftBracket = '(';
@@ -126,6 +127,7 @@
             this.RightBox.Cursor = System.Windows.Forms.Cursors.IBeam;
             this.RightBox.DisabledColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))));
             this.RightBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.RightBox.Font = new System.Drawing.Font("Courier New", 9.75F);
             this.RightBox.IsReplaceMode = false;
             this.RightBox.Language = FastColoredTextBoxNS.Language.CSharp;
             this.RightBox.LeftBracket = '(';
@@ -144,12 +146,23 @@
             // 
             // TopPanel
             // 
+            this.TopPanel.Controls.Add(this.OpenButton);
             this.TopPanel.Controls.Add(this.ConversionButton);
             this.TopPanel.Dock = System.Windows.Forms.DockStyle.Top;
             this.TopPanel.Location = new System.Drawing.Point(0, 0);
             this.TopPanel.Name = "TopPanel";
             this.TopPanel.Size = new System.Drawing.Size(908, 49);
             this.TopPanel.TabIndex = 3;
+            // 
+            // OpenButton
+            // 
+            this.OpenButton.Location = new System.Drawing.Point(244, 12);
+            this.OpenButton.Name = "OpenButton";
+            this.OpenButton.Size = new System.Drawing.Size(75, 23);
+            this.OpenButton.TabIndex = 3;
+            this.OpenButton.Text = "Open";
+            this.OpenButton.UseVisualStyleBackColor = true;
+            this.OpenButton.Click += new System.EventHandler(this.OpenButton_Click);
             // 
             // ConversionButton
             // 
@@ -160,6 +173,11 @@
             this.ConversionButton.Text = "Conversion: Apex → C# (click to swap)";
             this.ConversionButton.UseVisualStyleBackColor = true;
             this.ConversionButton.Click += new System.EventHandler(this.ConversionButton_Click);
+            // 
+            // OpenFileDialog
+            // 
+            this.OpenFileDialog.FileName = "Demo.cls";
+            this.OpenFileDialog.Filter = "Apex (*.cls)|*.cls|C# (*.cs)|*.cs|All files (*.*)|*.*";
             // 
             // ConvertorForm
             // 
@@ -189,6 +207,8 @@
         private FastColoredTextBoxNS.FastColoredTextBox RightBox;
         private System.Windows.Forms.Panel TopPanel;
         private System.Windows.Forms.Button ConversionButton;
+        private System.Windows.Forms.Button OpenButton;
+        private System.Windows.Forms.OpenFileDialog OpenFileDialog;
     }
 }
 
--- Playground/ConvertorForm.cs
+++ Playground/ConvertorForm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -62,6 +63,14 @@ namespace Playground
 
             LeftBox.Text = RightBox.Text;
             DoConvert();
+        }
+
+        private void OpenButton_Click(object sender, EventArgs e)
+        {
+            if (OpenFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                LeftBox.Text = File.ReadAllText(OpenFileDialog.FileName);
+            }
         }
     }
 }
