20441ab3d5ced06b2112d60dd809aef9b856ab7f
yallie <yallie@yandex.ru>
2017-11-30 01:06:54 +0300

    Uncommented the initializer in SoqlDemo. Fixed a bug in JsonExample class.

--- ApexSharpDemo/CSharpClasses/JsonExample.cs
+++ ApexSharpDemo/CSharpClasses/JsonExample.cs
@@ -14,7 +14,7 @@ namespace ApexSharpDemo.CSharpClasses
             contact.LastName = "Jay";
             contact.Email = "jay@jay.com";
             string jsonString = JSON.Serialize(contact);
-            Contact newContact = (Contact)JSON.Deserialize(jsonString, typeof(string));
+            Contact newContact = (Contact)JSON.Deserialize(jsonString, typeof(Contact));
         }
     }
 }
