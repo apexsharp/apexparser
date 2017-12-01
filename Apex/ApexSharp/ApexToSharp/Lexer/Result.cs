5342e4bc82e5657b32eb5a0f2a11b6807c8da20f
Jay Janarthanan <jay@jayonsoftware.com>
2017-12-01 07:49:00 -0500

    Code Cleanup

--- Apex/ApexSharp/ApexToSharp/Lexer/Result.cs
+++
@@ -1,9 +0,0 @@
-namespace Apex.ApexSharp.ApexToSharp.Lexer
-{
-    public class Result
-    {
-        public bool IsGood { get; set; }
-        public TockenType TokenType { get; set; }
-        public string TokenContents { get; set; }
-    }
-}
