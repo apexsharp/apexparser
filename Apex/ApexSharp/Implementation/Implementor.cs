c4bedebb1cf720d1dd5b45aa7a76c30b9a920bd8
yallie <yallie@yandex.ru>
2017-12-12 23:25:19 +0300

    Added the API implementor class and its unit tests.

--- Apex/ApexSharp/Implementation/Implementor.cs
+++ Apex/ApexSharp/Implementation/Implementor.cs
@@ -1,10 +1,68 @@
-﻿namespace Apex.ApexSharp
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using Type = System.Type;
+
+namespace Apex.ApexSharp.Implementation
 {
 	public class Implementor
 	{
-		public static dynamic GetImplementation(global::System.Type type)
-		{
-			return null;
-		}
-	}
+        public static ConcurrentDictionary<Type, dynamic> ImplementationRepository { get; private set; } =
+            new ConcurrentDictionary<Type, dynamic>();
+
+        public static dynamic GetDefaultImplementation(Type type) =>
+            new StubImplementation(type.FullName);
+
+        public static dynamic GetImplementation(Type type)
+        {
+            if (ImplementationRepository.TryGetValue(type, out var result))
+            {
+                return result;
+            }
+
+            var impl = GetDefaultImplementation(type);
+            ImplementationRepository[type] = impl;
+            return impl;
+        }
+
+        public static void SetImplementation(Type type, dynamic stub)
+        {
+            ImplementationRepository[type] = stub;
+        }
+
+        public static void ResetToDefault()
+        {
+            ImplementationRepository.Clear();
+        }
+
+        public static IDisposable UseDefaultImplementations()
+        {
+            var oldRepository = ImplementationRepository;
+            ImplementationRepository = new ConcurrentDictionary<Type, dynamic>();
+
+            return new Disposable(() =>
+            {
+                ImplementationRepository = oldRepository;
+            });
+        }
+
+        public static IDisposable UseDefaultImplementation<T>() => UseDefaultImplementation(typeof(T));
+
+        public static IDisposable UseDefaultImplementation(Type type)
+        {
+            var oldExisted = ImplementationRepository.TryRemove(type, out dynamic old);
+
+            return new Disposable(() =>
+            {
+                if (oldExisted)
+                {
+                    ImplementationRepository[type] = old;
+                }
+                else
+                {
+                    ImplementationRepository.TryRemove(type, out var tmp);
+                }
+            });
+        }
+    }
 }
