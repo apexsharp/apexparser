1b0d80c62d206c2503ef0d0b60d2f0dd72d55152
yallie <yallie@yandex.ru>
2018-08-05 21:06:09 +0300

    Added AST node classes for the switch-when statement.

---
+++ ApexParser/MetaClass/WhenElseClauseSyntax.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ApexParser.Visitors;
+
+namespace ApexParser.MetaClass
+{
+    public class WhenElseClauseSyntax : WhenClauseSyntax
+    {
+        public override SyntaxType Kind => SyntaxType.WhenElseClause;
+
+        public override void Accept(ApexSyntaxVisitor visitor) => visitor.VisitWhenElseClauseSyntax(this);
+    }
+}
---
+++ ApexParser/MetaClass/WhenTypeClauseSyntax.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ApexParser.Visitors;
+
+namespace ApexParser.MetaClass
+{
+    public class WhenTypeClauseSyntax : WhenClauseSyntax
+    {
+        public override SyntaxType Kind => SyntaxType.WhenTypeClause;
+
+        public override void Accept(ApexSyntaxVisitor visitor) => visitor.VisitWhenTypeClauseSyntax(this);
+
+        public override IEnumerable<BaseSyntax> ChildNodes => GetNodes(Type, Statement);
+
+        public TypeSyntax Type { get; set; }
+
+        public string Identifier { get; set; }
+    }
+}
